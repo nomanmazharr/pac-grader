887c5744f38d2aea726c8a11b661755f3668cd70
Noman <nmazhar911@gmail.com>
2025-10-18 07:30:25 +0500

    using fine-tuned model

--- llm_setup.py
+++ llm_setup.py
@@ -10,20 +10,20 @@ together_api = os.getenv("TOGETHER_API_KEY")
 openai_api = os.getenv("OPENAI_API_KEY")
 
 
-# llm_grader = ChatTogether(
-#     # model="openai/gpt-oss-20b",
-#     model = "talhapacahmed_9091/gpt-oss-20b-78eb5223-03bd6cc1",
-#     temperature=0,
-#     api_key= together_api,
-#     max_tokens=80000
-# )
-
 llm_grader = ChatTogether(
-    model="openai/gpt-oss-20b",
+    # model="openai/gpt-oss-20b",
+    model = "talhapacahmed_9091/gpt-oss-20b-78eb5223-03bd6cc1",
     temperature=0,
     api_key= together_api,
     max_tokens=80000
 )
+
+# llm_grader = ChatTogether(
+#     model="openai/gpt-oss-20b",
+#     temperature=0,
+#     api_key= together_api,
+#     max_tokens=80000
+# )
 
 llm = ChatTogether(
     model="openai/gpt-oss-20b",
